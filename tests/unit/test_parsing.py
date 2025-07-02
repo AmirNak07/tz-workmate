c42f1855f3b0ceecc7cc98d4dc8e908e9930a8dc
amirnak <amirnakgamer@gmail.com>
2025-07-02 21:35:51 +0300

    Add: unit tests for parsing 'aggregate' flag

--- tests/unit/test_parsing.py
+++ tests/unit/test_parsing.py
@@ -2,7 +2,7 @@ from contextlib import nullcontext as does_not_raise
 
 import pytest
 
-from main import parse_where_param
+from main import parse_aggregate_param, parse_where_param
 
 
 @pytest.mark.parametrize(
@@ -28,3 +28,22 @@ def test_parse_where_param(
         assert col == expected_col
         assert op == expected_op
         assert val == expected_val
+
+
+@pytest.mark.parametrize(
+    "input_str,expected_col,expected_op, expected_err",
+    [
+        ("price=min", "price", "min", does_not_raise()),
+        ("price=max", "price", "max", does_not_raise()),
+        ("price=avg", "price", "avg", does_not_raise()),
+        ("price=test", "price", "test", does_not_raise()),
+        ("price>avg", "price", "avg", pytest.raises(ValueError)),
+        ("price", "price", "", pytest.raises(ValueError)),
+        ("=", "", "", pytest.raises(ValueError)),
+    ],
+)
+def test_parse_aggregate_param(input_str, expected_col, expected_op, expected_err):
+    with expected_err:
+        col, op = parse_aggregate_param(input_str)
+        assert col == expected_col
+        assert op == expected_op
