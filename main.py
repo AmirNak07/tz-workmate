64927f8fa45a986c249c862e9af7dc11cbdb9960
amirnak <amirnakgamer@gmail.com>
2025-07-03 00:40:49 +0300

    Fix: Delete arg 'dest' in main parser

--- main.py
+++ main.py
@@ -93,14 +93,12 @@ def apply_aggregation(
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Process CSV file.")
+    parser.add_argument("--file", help="Path to CSV file", required=True)
     parser.add_argument(
-        "--file", help="Path to CSV file", required=True, dest="<FILE_PATH>"
+        "--where", help='Filter condition (e.g., "price>500")', type=str
     )
     parser.add_argument(
-        "--where", help='Filter condition (e.g., "price>500")', type=str, dest=""
-    )
-    parser.add_argument(
-        "--aggregate", help='Aggregate orepation (e.g., "avg=price")', type=str, dest=""
+        "--aggregate", help='Aggregate orepation (e.g., "avg=price")', type=str
     )
 
     args = parser.parse_args()
